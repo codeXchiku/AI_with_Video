089eb2919e1d1540713cbe2e5f89f791fb4bce2f
web9bariflo <web9.bariflo@outlook.com>
2025-09-16 18:05:18 +0530

    imagekit and video upload logic work completed

---
+++ app/api/auth/imagekit-auth/route.ts
@@ -0,0 +1,23 @@
+import { getUploadAuthParams } from "@imagekit/next/server"
+
+export async function GET() {
+
+    try {
+        const authenticationParameter = getUploadAuthParams({
+            privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
+            publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
+        })
+
+        return Response.json({
+            authenticationParameter,
+            publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY
+        })
+    } catch (error) {
+        return Response.json(
+            {
+                error: "Authentication for imagekit failed"
+            },
+            { status: 500 }
+        )
+    }
+}
